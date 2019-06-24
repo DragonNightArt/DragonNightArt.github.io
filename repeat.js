0c1233e47f02409db0e7ac64e571ed7f48e0bfd8
Gabriel Villaschi <4luxxx@gmail.com>
2019-06-24 20:03:47 +0100

    refactored .del function
    will work for any button with a .del class inside a section with a .repeating class

--- repeat.js
+++ repeat.js
@@ -27,50 +27,13 @@ $('.add').click(function(e) {
 
 // Delete
 $('.del').click(function(e){
-		alert('delete');
         e.preventDefault();
-        var current_set = $(this).parent('div');
-        var other_set = current_set.siblings('.repeatingGroup');
-        if (other_set.length === 0) {
-            alert("Cannot delete last entry");
-            return;
-        }
-        current_set.slideUp('slow', function() {
-            current_set.remove();
 
-            // reset indexes
-            other_set.each(function() {
-               resetAttributeNames($(this)); 
-            })  
-        })
-    });
+        var classes = this.classList;
+        var target = ".repeating" + classes[0];
 
-//Collapse
-$('.hide').click(function(e) {
-		alert('hide');
-		this.classList.toggle("active");
-		var content = this.nextElementSibling;
-		if (content.style.display === "block") {
-			content.style.display = "none";
-		} else {
-			content.style.display = "block";
-		}
-	});
-//		ITEMS
-//Add
-$('.itemAdd').click(function(e){
-        e.preventDefault();
-        var lastRepeatingGroup = $('.repeatingItems').last();
-        var cloned = lastRepeatingGroup.clone(true)  
-        cloned.insertAfter(lastRepeatingGroup);
-        resetAttributeNames(cloned)
-    });
-                    
-// Delete
-$('.itemDel').click(function(e){
-        e.preventDefault();
         var current_item = $(this).parent('div');
-        var other_items = current_item.siblings('.repeatingItems');
+        var other_items = current_item.siblings(target);
         if (other_items.length === 0) {
             alert("Cannot delete last entry");
             return;
@@ -85,37 +48,9 @@ $('.itemDel').click(function(e){
         })
     });
 
-//		GEAR
-// Add
-$('.gearAdd').click(function(e){
-        e.preventDefault();
-        var lastRepeatingGroup = $('.repeatingGear').last();
-        var cloned = lastRepeatingGroup.clone(true)  
-        cloned.insertAfter(lastRepeatingGroup);
-        resetAttributeNames(cloned)
-    });
-                    
-// Delete
-$('.gearDel').click(function(e){
-        e.preventDefault();
-        var current_gear = $(this).parent('div');
-        var other_gear = current_gear.siblings('.repeatingGear');
-        if (other_gear.length === 0) {
-            alert("Cannot delete last entry");
-            return;
-        }
-        current_gear.slideUp('slow', function() {
-            current_gear.remove();
-            
-            // reset indexes
-            other_gear.each(function() {
-               resetAttributeNames($(this)); 
-            })  
-        })
-    });
-
 //Collapse
-$('.gearHide').click(function(e) {
+$('.hide').click(function(e) {
+		alert('hide');
 		this.classList.toggle("active");
 		var content = this.nextElementSibling;
 		if (content.style.display === "block") {
